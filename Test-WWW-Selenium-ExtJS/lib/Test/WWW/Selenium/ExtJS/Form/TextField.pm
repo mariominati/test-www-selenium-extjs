7f5bf38ead89f14cdf668a1b04814163227d6286
Mario Minati <mario.minati@minati.de>
2010-01-06 22:58:24 +0100

    added xtype and method for typing text

--- Test-WWW-Selenium-ExtJS/lib/Test/WWW/Selenium/ExtJS/Form/TextField.pm
+++ Test-WWW-Selenium-ExtJS/lib/Test/WWW/Selenium/ExtJS/Form/TextField.pm
@@ -5,8 +5,14 @@ use Moose;                                       # Includes strict and warnings
 extends "Test::WWW::Selenium::ExtJS::Form::Field";
 
 use Readonly;
-Readonly my $TRUE       => 1;
-Readonly my $FALSE      => 0;
+Readonly my $TRUE   => 1;
+Readonly my $FALSE  => 0;
+
+
+# xtype - set the default xtype of this Ext component
+has '+xtype' => (
+    default => 'textfield',
+);
 
 
 # Build component from given parameters
@@ -18,7 +24,13 @@ sub BUILD {
 
 
 # Add specific methods here
+sub type_value {
+    my $self = shift;
+    my ( $value ) = @_;
 
+    # Return for chaining
+    return $self->wait_for_rendered->focus->type( $value );
+}
 
 1;  # Magic true value required at end of module
 __END__
