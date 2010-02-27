2d524248cc3eac5902b0cef063d41dfe0ba6de88
Mario Minati <mario.minati@minati.de>
2010-02-27 12:38:18 +0100

    added more docs

--- Test-WWW-Selenium-ExtJS/lib/Test/WWW/Selenium/ExtJS/Layout.pm
+++ Test-WWW-Selenium-ExtJS/lib/Test/WWW/Selenium/ExtJS/Layout.pm
@@ -21,7 +21,7 @@ __END__
 
 =head1 NAME
 
-Test::WWW::Selenium::ExtJS::Layout::BorderLayout - Proxy class for Ext.layout.BorderLayout
+Test::WWW::Selenium::ExtJS::Layout - Base class for all layouts
 
 
 =head1 SYNOPSIS
@@ -31,117 +31,22 @@ Look at L<Test::WWW::Selenium::ExtJS>.
   
 =head1 DESCRIPTION
 
-This is the proxy class for the Ext.layout.BorderLayout object and extends the 
-L<Test::WWW::Selenium::ExtJS::Layout> object.
-L<http://www.extjs.com/deploy/dev/docs/?class=Ext.layout.BorderLayout>
+This is the base class for all the Ext.layout.* classes and extends the 
+L<Test::WWW::Selenium::ExtJS::Expression> class.
 
 
 =head1 INTERFACE 
 
-
-###
-### rework from here
-###
-
-
 =head2 Attributes
 
-=head3 C<parent>
+The attributes of the base class are described at 
+L<Test::WWW::Selenium::ExtJS::Expression>.
 
-Type: C<Test::WWW::Selenium::ExtJS::Component> object.
-
-Proxy for the containing ExtJS component.
-
-We need either the L<parent> or the L<extjs> attribute to connect to the 
-selenium object.
-
-=head3 C<extjs>
-
-Type: C<Test::WWW::Selenium::ExtJS> object.
-
-The central ExtJS object.
-
-We need either the L<parent> or the L<extjs> attribute to connect to the 
-selenium object.
-
-=head3 C<expression>
-
-Type: C<Str>.
-
-The JavaScript expression that will be evaluated to access the ExtJS component
-in selenium.
-
-If L<expression> is not given, we need the L<id> attribute.
-
-=head3 C<id>
-
-Type: C<Str>.
-
-Sets the id of the Ext component. This is usually used for windows, viewports
-and other objects with fixed id.
-
-If L<id> is not given, we need the L<expression> attribute.
-
-=head3 C<xtype>
-
-Type: C<Str>.
-
-The XType of the Ext component, default is 'component'.
 
 =head2 General methods
 
-=head3 C<get_id>
-
-Returns the ID of the Ext component, found with the proxy's JS expression. 
-
-=head3 C<get_xpath>
-
-Returns an XPath to the Ext component, which contains the ID provided 
-by L<get_id>.
-
-=head3 C<get_expression>
-
-Returns the absolute expression that resolves this proxy's Ext component.
-
-=head3 C<get_eval_on_component>
-
-Evaluates expression on this component.
-
-=head2 Component methods to synchronise with AJAX
-
-=head3 C<wait_eval_on_component_true>
-
-Waits until the expression for this component evals true, dies on timeout.
-
-=head2 Convenience methods to evaluate properties
-
-=head3 C<get_eval_component_string_property>
-
-Gets a property from the Ext component as string.
-
-=head3 C<get_eval_component_property_exists>
-
-Returns true if the requested property exists in the Ext component.
-
-=head3 C<get_eval_component_boolean_property>
-
-Gets a property from the Ext component as boolean value.
-If the property equals 'true' we return a true value.
-
-=head2 Methods to synchronise with AJAX
-
-=head3 C<wait_for_component>
-
-Does a wait loop until the component is available.
-
-=head3 C<wait_for_rendered>
-
-Does a wait loop until the component is rendered.
-
-=head3 C<is_enabled>
-
-Returns true if the 'disabled' property of the component is B<not> set,
-false otherwise.
+The methods of the base class are described at 
+L<Test::WWW::Selenium::ExtJS::Expression>.
 
 
 =head1 DIAGNOSTICS
